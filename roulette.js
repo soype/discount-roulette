3fcb3bcf37d7d605c3873becd94955708a79de6b
Pedro Henestroza <estudio@192.168.1.24>
2025-03-20 22:24:23 -0300

    Add mobile and cookies

--- roulette.js
+++ roulette.js
@@ -1,16 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".modal").style.opacity = 1;
+  document.querySelector(".modal").style.opacity = 1;
   document.querySelector(".modal__backdrop").addEventListener("click", disableModal);
+
+  if (document.cookie.includes("successCoupon")) {
+    const cookie = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("successCoupon="));
+    if (cookie) {
+      const result = cookie.split("=")[1];
+      const [coupon, option] = result.split("__");
+      const selectedOption = document.querySelector(`#option-${option}`);
+      if (selectedOption) {
+        showSuccess(selectedOption, coupon, false);
+      }
+    }
+  }
 });
 
 const disableModal = () => {
-    document.querySelector(".modal").style.opacity = 0;
-    document.querySelector(".modal__backdrop").style.opacity = 0;
-    setTimeout(() => {
-      document.querySelector(".modal").style.display = "none";
-      document.querySelector(".modal__backdrop").style.display = "none";
-    }, 300);
-}
+  document.querySelector(".modal").style.opacity = 0;
+  document.querySelector(".modal__backdrop").style.opacity = 0;
+  setTimeout(() => {
+    document.querySelector(".modal").style.display = "none";
+    document.querySelector(".modal__backdrop").style.display = "none";
+  }, 300);
+};
 
 const wheel = document.querySelector(".wheel");
 const options = wheel.querySelector(".wheel__options");
@@ -35,18 +49,33 @@ function rotateWheel() {
   }
   options.style.transform = `rotate(${currentRotate + rotate}deg)`;
 
-  button.disabled = true;
-  button.style.zIndex = 4;
-  document.querySelector(".wheel__final").style.opacity = 1;
-  document.querySelector(".modal__post").style.opacity = 1;
-
   const finalOption = Math.floor((rotate - timesRotated * 1080 - 720) / 45) + 1;
   const selectedOption = document.getElementById(`option-${finalOption}`);
 
   const coupon = coupons[finalOption - 1];
 
+  showSuccess(selectedOption, coupon, true);
+
+  document.cookie = `successCoupon=${coupon}__${finalOption};max-age=31536000;path=/`; 
+
+}
+
+function showSuccess(selectedOption, coupon, firstTime = true) {
   document.querySelector("#success").innerHTML = selectedOption.children[0].innerHTML;
   document.querySelector(".wheel__final-coupon").innerHTML = coupon;
+
+  button.disabled = true;
+  button.style.zIndex = 4;
+  if (!firstTime) {
+    document.querySelector('.wheel__options').style.opacity = 0;
+    document.querySelector(".wheel__final").style.opacity = 1;
+    document.querySelector(".modal__post").style.opacity = 1;
+  }else{
+    setTimeout(() => {
+    document.querySelector(".wheel__final").style.opacity = 1;
+    document.querySelector(".modal__post").style.opacity = 1;
+    }, 3500)
+  }
 }
 
 button.addEventListener("click", rotateWheel);
